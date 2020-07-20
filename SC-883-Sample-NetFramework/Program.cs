3d82eae3b9dd29fd73a1d052bd796bd5e4c61fbe
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-07-20 14:52:50 +0200

    Add project files.

---
+++ SC-883-Sample-NetFramework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text; // S1128 is raised here
+
+namespace SC_883_Sample_NetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Siegfried is the best!");
+        }
+    }
+}
